94864c415cdf5f44cb199d0b6b04022307526672
Bach <53639538+PhamQuangBach@users.noreply.github.com>
2026-05-16 17:36:42 +0300

    c

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/pdf-editor",   // change to "/your-repo-name/" for GitHub Pages
+  base: "/pdf-editor/",   // change to "/your-repo-name/" for GitHub Pages
 });
